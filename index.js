7c2541e8eec17c680918dce83c70d3c403066fa8
Rudra <rudramittal24@gmail.com>
2024-02-09 17:43:19 +0530

    Created basic layout of home page and some get APIs

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const Listing= require("./models/listingModel.js");
+const path=require("path");
 const mongoURL="mongodb://127.0.0.1:27017/airbnb"
 const app=express();
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"/public/styles")));
+app.use(express.static(path.join(__dirname,"/public/scripts")));
+app.use(express.urlencoded({extended:true}));
+app.set("view engine","ejs");
 const port=8080;
 main()
 .then(()=>{
@@ -18,19 +24,15 @@ async function main(){
 app.get("/",(req,res)=>{
     res.send("you are on home page");
 })
-app.get("/testListing",async(req,res)=>{
-    let newListing= new Listing({
-        title:"Villa 1",
-        description:"Located riverside, Amazing view",
-        price:"2500000",
-        location:"Dharavi,Mumbai",
-        country:"India"
-    })
-    await newListing.save();
-    res.send("saved");
+app.get("/listings",async(req,res)=>{
+    let listings=await Listing.find();
+    res.render("index.ejs",{listings});
 })
-
-
+app.get("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    let listing=await Listing.findById(id);
+    res.render("show.ejs",{listing});
+})
 
 
 
