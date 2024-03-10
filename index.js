7f65872fe93ea4def3f644b7a786ec9b4efdf116
Rudra <rudramittal24@gmail.com>
2024-03-10 07:12:00 +0530

    Error Handling middleware added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Listing= require("./models/listingModel.js");
 const path=require("path");
 const mongoURL="mongodb://127.0.0.1:27017/airbnb"
 const app=express();
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
 engine=require("ejs-mate");
 app.engine("ejs",engine);
 app.set("views",path.join(__dirname,"/views"));
@@ -28,30 +30,33 @@ async function main(){
 app.get("/",(req,res)=>{
     res.send("you are on home page");
 })
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
     let listings=await Listing.find();
     res.render("listings/index.ejs",{listings});
-})
+}));
 
-app.get("/listings/:id/update",async(req,res)=>{
+app.get("/listings/:id/update",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let listing=await Listing.findById(id);
     res.render("listings/update.ejs",{listing});
-})
-app.put("/listings/:id/update",async(req,res)=>{
+}))
+app.put("/listings/:id/update",wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"No/incomplete data Entered for listings");
+    }
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-})
+}));
 app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs");
 })
-app.delete("/listings/:id/delete",async(req,res)=>{
+app.delete("/listings/:id/delete",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-})
-app.post("/listings/new",async(req,res)=>{
+}));
+app.post("/listings/new",wrapAsync(async(req,res,next)=>{
     // let {title,price,description,location,country,image}=req.body;
     // console.log("new creating request received",req.body);
     // let listing= new Listing({
@@ -63,17 +68,26 @@ app.post("/listings/new",async(req,res)=>{
     //     image:`${image}`
     // })
     // OR
-    // console.log(req.body);
-    const listing = new Listing(req.body.listing);
-    await listing.save();
-    res.redirect("/listings");
-})
-app.get("/listings/:id",async(req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"No/incomplete data Entered for listings");
+    }
+        const listing = new Listing(req.body.listing);
+        await listing.save();
+        res.redirect("/listings");
+}));
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let listing= await Listing.findById(id);
      res.render("listings/show.ejs",{listing});
+}));
+app.all("*",(req,res,next)=>{
+    next( new ExpressError(404,"Page Not Found :("));
 })
-
 app.listen(port,(req,res)=>{
     console.log(`app is listening on port${port}`);
+})
+app.use((err,req,res,next)=>{
+    let{status=500,message="Something went Wrong"}=err;
+    res.render("listings/error.ejs",{status,message});
+    // res.status(status).send(message);
 })
